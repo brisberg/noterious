e53a7292866815d672dde8230ff2b05ea5045661
Brandon Risberg <brandon.risberg@gmail.com>
2016-03-26 16:50:35 -0700

    Part way on the third challenge.

--- src/app/boards/boards-controller.js
+++ src/app/boards/boards-controller.js
@@ -1,84 +1,77 @@
 'use strict';
 
 angular.module('noterious')
-  .controller('BoardsCtrl', function (currentUser, BoardsModel) {
-    var ctrl = this;
+    .controller('BoardsCtrl', function (currentUser, BoardsModel) {
+        var ctrl = this;
 
-    ctrl.loading = false;
+        ctrl.loading = false;
 
-    ctrl.newBoard = {
-      title: '',
-      description: '',
-      isPublic: false
-    };
+        ctrl.newBoard = {
+            title: '',
+            description: '',
+            isPublic: false
+        };
 
-    ctrl.resetForm = function () {
-      ctrl.loading = false;
-      ctrl.newBoard = {
-        title: '',
-        description: '',
-        isPublic: false
-      };
-    };
+        ctrl.resetForm = function () {
+            ctrl.loading = false;
+            ctrl.newBoard = {
+                title: '',
+                description: '',
+                isPublic: false
+            };
+        };
 
-    ctrl.getBoards = function () {
-      ctrl.boards = {
-        1: {
-          description: "Anything and everything!",
-          isPublic: true,
-          title: "Random Ideas"
-        },
-        2: {
-          description: "BizDev Ideas",
-          isPublic: false,
-          title: "Hustle"
-        },
-        3: {
-          description: "this is a test",
-          isPublic: false,
-          title: "testing"
-        }
-      };
-    };
+        ctrl.getBoards = function () {
+            BoardsModel.all()
+                .then(function (boards) {
+                    ctrl.boards = boards;
+                })
+                .catch(function (reason) {
+                    //
+                })
+                .finally(function () {
+                    //
+                });
+        };
 
-    ctrl.createBoard = function (board, isValid) {
-      if (isValid) {
-        ctrl.loading = true;
-        // CREATE BOARD
-        ctrl.resetForm();
-      }
-    };
+        ctrl.createBoard = function (board, isValid) {
+            if (isValid) {
+                ctrl.loading = true;
+                // CREATE BOARD
+                ctrl.resetForm();
+            }
+        };
 
-    ctrl.updateBoard = function (boardId, board, isValid) {
-      if (isValid) {
-        ctrl.loading = true;
-        // UPDATE BOARD
-        ctrl.cancelEditing();
-      }
-    };
+        ctrl.updateBoard = function (boardId, board, isValid) {
+            if (isValid) {
+                ctrl.loading = true;
+                // UPDATE BOARD
+                ctrl.cancelEditing();
+            }
+        };
 
-    ctrl.deleteBoard = function (boardId) {
-      ctrl.loading = true;
-      // DELETE BOARD
-      ctrl.cancelEditing();
-    };
+        ctrl.deleteBoard = function (boardId) {
+            ctrl.loading = true;
+            // DELETE BOARD
+            ctrl.cancelEditing();
+        };
 
-    ctrl.setEditedBoard = function (boardId, board) {
-      ctrl.editedBoardId = boardId;
-      ctrl.editedBoard = angular.copy(board);
-      ctrl.isEditing = true;
-    };
+        ctrl.setEditedBoard = function (boardId, board) {
+            ctrl.editedBoardId = boardId;
+            ctrl.editedBoard = angular.copy(board);
+            ctrl.isEditing = true;
+        };
 
-    ctrl.isCurrentBoard = function (boardId) {
-      return ctrl.editedBoard !== null && ctrl.editedBoardId === boardId;
-    };
+        ctrl.isCurrentBoard = function (boardId) {
+            return ctrl.editedBoard !== null && ctrl.editedBoardId === boardId;
+        };
 
-    ctrl.cancelEditing = function () {
-      ctrl.loading = false;
-      ctrl.editedBoardId = null;
-      ctrl.editedBoard = null;
-      ctrl.isEditing = false;
-    };
+        ctrl.cancelEditing = function () {
+            ctrl.loading = false;
+            ctrl.editedBoardId = null;
+            ctrl.editedBoard = null;
+            ctrl.isEditing = false;
+        };
 
-    ctrl.getBoards();
-  });
+        ctrl.getBoards();
+    });
