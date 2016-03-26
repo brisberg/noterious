56fca46e307026806b6b0f66acc20839c82bd43b
Brandon Risberg <brandon.risberg@gmail.com>
2016-03-26 14:32:38 -0700

    first challenge minus the test

---
+++ src/app/boards/boards-controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('noterious')
+    .controller('BoardsCtrl', function (UserModel, $state) {
+        var boards = this;
+
+        boards.title = 'BOARDS!';
+
+    });
--- src/app/noterious.js
+++ src/app/noterious.js
@@ -1,38 +1,41 @@
 'use strict';
 
 angular.module('noterious', [
-  'ui.router',
-  'ngAnimate',
-  'firebase',
-  'noterious.common'
+    'ui.router',
+    'ngAnimate',
+    'firebase',
+    'noterious.common'
 ])
-  .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
-  .config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/login');
+    .constant('ENDPOINT_URI', 'https://noterious.firebaseio.com/')
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
 
-    $stateProvider
-      .state('login', {
-        url:'/login',
-        templateUrl: 'app/login/login.tmpl.html',
-        controller: 'LoginCtrl',
-        controllerAs: 'login'
-      })
-    ;
-
-    /* HINT: Add this to your boards route to force authentication
-     resolve: {
-       'currentUser': ['Auth', function (Auth) {
-        return Auth.$requireAuth();
-       }]
-     }
-     */
-  })
-  .run(function ($rootScope, $state) {
-    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
-      event.preventDefault();
-      if (error === 'AUTH_REQUIRED') {
-        $state.go('login');
-      }
-    });
-  })
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'app/login/login.tmpl.html',
+                controller: 'LoginCtrl',
+                controllerAs: 'login'
+            })
+            .state('boards', {
+                url: '/boards',
+                templateUrl: 'app/boards/boards.tmpl.html',
+                controller: 'BoardsCtrl',
+                controllerAs: 'boards',
+                resolve: {
+                    'currentUser': ['Auth', function (Auth) {
+                        return Auth.$requireAuth();
+                    }]
+                }
+            })
+        ;
+    })
+    .run(function ($rootScope, $state) {
+        $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+            event.preventDefault();
+            if (error === 'AUTH_REQUIRED') {
+                $state.go('login');
+            }
+        });
+    })
 ;
