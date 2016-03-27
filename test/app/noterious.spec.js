c17f1994f5edf1bd613b259883eadb2859760b40
Brandon Risberg <brandon.risberg@gmail.com>
2016-03-27 14:18:13 -0700

    Test for 1st challenge

--- test/app/noterious.spec.js
+++ test/app/noterious.spec.js
@@ -2,23 +2,39 @@
 
 describe('Noterious: configuration', function () {
 
-  beforeEach(module('noterious'));
+    beforeEach(module('noterious'));
 
-  describe('Constant: noterious firebase endpoint', function () {
+    describe('Constant: noterious firebase endpoint', function () {
 
-    var endpointUri;
+        var endpointUri;
 
-    beforeEach(inject(function (_ENDPOINT_URI_) {
-      endpointUri = _ENDPOINT_URI_;
-    }));
+        beforeEach(inject(function (_ENDPOINT_URI_) {
+            endpointUri = _ENDPOINT_URI_;
+        }));
 
-    it('should be defined', function () {
-      expect(endpointUri).toBeDefined();
+        it('should be defined', function () {
+            expect(endpointUri).toBeDefined();
+        });
+
+        it('should be secure', function () {
+            expect(endpointUri).toContain("https://");
+        });
+
     });
 
-    it('should be secure', function () {
-      expect(endpointUri).toContain("https://");
-    });
+    describe('Boards Route', function () {
+        var $state,
+            $rootScope,
+            state = 'boards';
 
-  });
+        beforeEach(inject(function (_$state_, $templateCache, _$rootScope_) {
+            $state = _$state_;
+            $rootScope = _$rootScope_;
+            $templateCache.put('app/boards/boards.tmpl.html', '');
+        }));
+
+        it('should respond to URL', function () {
+            expect($state.href(state)).toEqual('#/boards');
+        });
+    });
 });
